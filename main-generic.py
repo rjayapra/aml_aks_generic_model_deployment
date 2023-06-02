5a8fe557288234eb4e2cbdb51a3db202845f19e5
Radhika Jayaprakash <85903942+rjayapra@users.noreply.github.com>
2023-06-02 15:00:05 -0400

    Update main-generic.py

--- main-generic.py
+++ main-generic.py
@@ -44,6 +44,19 @@ from azureml.core.authentication import ServicePrincipalAuthentication
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
+import string
+import re
+import os
+import numpy as np
+import pandas as pd
+import urllib.request
+import json
+import keras
+from keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from azureml.core.model import Model
+import onnxruntime
+
 
 # In[ ]:
 
@@ -61,21 +74,95 @@ ws = Workspace.get(name=os.environ['WORKSPACE_NAME'],
 
 
 # In[ ]:
+def init(onnx_model_name):
 
-
-def score(raw_data, model_name):
-    # Get predictions and explanations for each data point
-    data = pd.read_json(raw_data)
+    global onnx_session
+    global dictonary
+    global contractions
     
-    model_path = Model.get_model_path(model_name, version=None, _workspace=ws)
+    try:
+        words_list_url = ('https://quickstartsws9073123377.blob.core.windows.net/'
+                          'azureml-blobstore-0d1c4218-a5f9-418b-bf55-902b65277b85/glove50d/wordsList.npy')
+        word_vectors_dir = './word_vectors'
+        os.makedirs(word_vectors_dir, exist_ok=True)
+        urllib.request.urlretrieve(words_list_url, os.path.join(word_vectors_dir, 'wordsList.npy'))
+        dictonary = np.load(os.path.join(word_vectors_dir, 'wordsList.npy'))
+        dictonary = dictonary.tolist()
+        dictonary = [word.decode('UTF-8') for word in dictonary]
+        print('Loaded the dictonary! Dictonary size: ', len(dictonary))
+        
+        contractions_url = ('https://quickstartsws9073123377.blob.core.windows.net/'
+                            'azureml-blobstore-0d1c4218-a5f9-418b-bf55-902b65277b85/glove50d/contractions.xlsx')
+        contractions_df = pd.read_excel(contractions_url)
+        contractions = dict(zip(contractions_df.original, contractions_df.expanded))
+        print('Loaded contractions!')
+        
+        # Retrieve the path to the model file using the model name
+        onnx_model_path = Model.get_model_path(onnx_model_name, version=None, _workspace=ws)
+        print('onnx_model_path: ', onnx_model_path)
+        
+        onnx_session = onnxruntime.InferenceSession(onnx_model_path)
+        print('Onnx Inference Session Created!')
+        
+    except Exception as e:
+        print(e)
 
-    model = joblib.load(model_path)
+def remove_special_characters(token):
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_token = pattern.sub('', token)
+    return filtered_token
 
-    # Make prediction
-    predictions = model.predict(data)
+def convert_to_indices(corpus, dictonary, c_map, unk_word_index = 399999):
+    sequences = []
+    for i in range(len(corpus)):
+        tokens = corpus[i].split()
+        sequence = []
+        for word in tokens:
+            word = word.lower()
+            if word in c_map:
+                resolved_words = c_map[word].split()
+                for resolved_word in resolved_words:
+                    try:
+                        word_index = dictonary.index(resolved_word)
+                        sequence.append(word_index)
+                    except ValueError:
+                        sequence.append(unk_word_index) #Vector for unkown words
+            else:
+                try:
+                    clean_word = remove_special_characters(word)
+                    if len(clean_word) > 0:
+                        word_index = dictonary.index(clean_word)
+                        sequence.append(word_index)
+                except ValueError:
+                    sequence.append(unk_word_index) #Vector for unkown words
+        sequences.append(sequence)
+    return sequences
 
-    # You can return any data type as long as it is JSON-serializable
-    return {'results': predictions.tolist()}
+def score(raw_data):
+    # Get predictions and explanations for each data point
+
+    try:
+        print("Received input: ", raw_data)
+        
+        maxSeqLength = 125
+        
+        print('Processing input...')
+        input_data_raw = [raw_data]
+        # input_data_raw = np.array(json.loads(raw_data))
+        input_data_indices = convert_to_indices(input_data_raw, dictonary, contractions)
+        input_data_padded = pad_sequences(input_data_indices, maxlen=maxSeqLength, padding='pre', truncating='post')
+        # convert the data type to float
+        input_data = np.reshape(input_data_padded.astype(np.float32), (1,maxSeqLength))
+        print('Done processing input.')
+        
+        # Run an ONNX session to classify the input.
+        result = onnx_session.run(None, {onnx_session.get_inputs()[0].name: input_data})[0].argmax(axis=1).item()
+        # return just the classification index (0 or 1)
+        return {'results': result}
+    except Exception as e:
+        print(e)
+        error = str(e)
+        return error
 
 
 # ### API
@@ -107,7 +194,8 @@ class Score(Resource):
         args = parser.parse_args()
         data = args['data']
         model_name = args['model_name']
-        results = score(data, model_name)
+        init(model_name)
+        results = score(data)
         return results, 200  # return data with 200 OK
 
 
@@ -135,7 +223,6 @@ if __name__ == '__main__':
 
 
 # In[ ]:
-
 
 
 
